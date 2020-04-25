fc93d2b4d50a1a8e7c5e434279409e2b3b6fc3d0
Archie Baer <archie@abaer.dev>
2020-04-25 03:22:21 +0100

    No clients message

--- pages/clients.js
+++ pages/clients.js
@@ -24,35 +24,39 @@ const ConnectPage = props => {
 		<Page user={props.user}>
 			<section>
 				<h1>Your Clients</h1>
-				<div className="clients">
-					{props.clients.map((c, i) => (
-						<article key={c.id}>
-							<div title={`Created at ${dates[i]}`}>
-								<h2>{c.name}</h2>
-							</div>
-							<div>
-								<Edit2 style={iconStyle} />
-								<X
-									style={iconStyle}
-									onClick={() => {
-										axios
-											.post(
-												`${config.apiUrl}/delete/${c.id}`,
-												{},
-												{
-													headers: {
-														authorization: props.user.sessionToken
+				{props.clients.length > 0 ? (
+					<div className="clients">
+						{props.clients.map((c, i) => (
+							<article key={c.id}>
+								<div title={`Created at ${dates[i]}`}>
+									<h2>{c.name}</h2>
+								</div>
+								<div>
+									<Edit2 style={iconStyle} />
+									<X
+										style={iconStyle}
+										onClick={() => {
+											axios
+												.post(
+													`${config.apiUrl}/delete/${c.id}`,
+													{},
+													{
+														headers: {
+															authorization: props.user.sessionToken
+														}
 													}
-												}
-											)
-											.then(() => location.reload())
-											.catch(() => {});
-									}}
-								/>
-							</div>
-						</article>
-					))}
-				</div>
+												)
+												.then(() => location.reload())
+												.catch(() => {});
+										}}
+									/>
+								</div>
+							</article>
+						))}
+					</div>
+				) : (
+					<p>You have no connected clients.</p>
+				)}
 			</section>
 
 			<style jsx>{`
@@ -65,8 +69,11 @@ const ConnectPage = props => {
 
 				h1 {
 					margin: 0;
-					margin-bottom: 30px;
 				}
+
+                .clients {
+					margin-top: 30px;
+                }
 
 				article {
 					display: flex;
